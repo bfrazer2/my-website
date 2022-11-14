475be618e7da2f00339222b46b1fd3e3fc55eacf
Benjamin Frazer <benjamin.frazer@benjamin.frazer-MacBook-Pro>
2022-11-14 16:27:00 -0500

    working slideshow added

--- photo-swapper.js
+++
@@ -1,9 +0,0 @@
-const images = ['images/Act%20Up.jpeg', 'images/soft.jpeg']
-let idx = 0; // starting index
-let maxImages = images.length - 1;
-const timer = setInterval(function() {
-    let curImage = images[index];
-    idx = (idx == maxImages) ? 0 : ++idx;
-    // set your image using the curImageVar 
-    $('#work-photo').attr('src',curImage);
- }, 1000);
